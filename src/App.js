e76638191991243752fa458cad3c954ea82358ff
Mathew <mmacelwee89@gmail.com>
2020-09-22 17:41:11 -0700

    continuing work on setting up layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar'
+import Chat from './Chat'
 
 function App() {
   return (
     <div className="app">
-      <h1> MERN Whatsapp clone</h1>
+      <div className='app__body'>
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
