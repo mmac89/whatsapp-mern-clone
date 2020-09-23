7a3e006da617a982a98128e3f34e8c3d9fcc6fd7
Mathew <mmacelwee89@gmail.com>
2020-09-22 18:23:47 -0700

    continuing working on app layout

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons'
+import React from 'react' 
 import './Chat.css'
 
 function Chat() {
     return (
         <div className='chat'>
-            <h1>chat</h1>
+            <div className='chat__header'>
+                <Avatar/>
+                <div className='chat__headerInfo'>
+                    <h3>Room Name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+
+                <div className='chat__headerRight'>
+                    <IconButton >
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton >
+                        <MoreVert />
+                    </IconButton>
+                </div>
+
+            </div>
+            <div className='chat__body' >
+                <p className='chat__message'>
+                    <span className='chat__name'> 
+                        Sonny
+                    </span>
+                    <span className='chat__timestamp'>   
+                        {new Date().toUTCString()}
+                    </span>
+                    This is a message...
+                </p>
+            </div>
         </div>
     )
 }
