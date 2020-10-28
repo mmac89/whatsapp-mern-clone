e835b1a7d6f5ef590b9e1abea472af86438c50a5
Mathew <mmacelwee89@gmail.com>
2020-10-28 14:57:59 -0700

    fixed logic in message sent/received

--- src/Chat.js
+++ src/Chat.js
@@ -1,166 +1,137 @@
-import React, { useState, useEffect } from 'react' 
-import './Chat.css'
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
 
-import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons'
-import InsertEmoticonIcon  from '@material-ui/icons/InsertEmoticon'
-import MicIcon from '@material-ui/icons/Mic'
-import Pusher from 'pusher-js'
-import axios from './axios'
-import { useParams } from 'react-router-dom';
-import { useStateValue } from './StateProvider'
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import Pusher from "pusher-js";
+import axios from "./axios";
+import { useParams } from "react-router-dom";
 
+function Chat({ user }) {
+  const [input, setInput] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [messages, setMessages] = useState([]);
+  const { roomId } = useParams();
 
-function Chat( {user} ) {
+  const [seed, setSeed] = useState("");
 
-    const [input, setInput] = useState('');
-    const [roomName, setRoomName] = useState('');
-    const [messages, setMessages] = useState([]);
-    const {roomId } =useParams();
+  useEffect(() => {
+    axios
+      .get(`/getRoomName/${roomId}`)
+      .then((response) => {
+        const room = response.data;
+        console.log(room);
+        setRoomName(room.roomName);
+        setSeed(Math.floor(Math.random() * 5000));
 
-    
-    const [seed, setSeed] =useState('');
-    useEffect(() => {
-        setSeed(Math.floor(Math.random() *5000)) 
-    }, [])
+        setMessages(room.roomMessages);
+      })
+      .catch(() => {
+        alert("error retrieving data");
+      });
+  }, [roomId]);
 
+  const sendMessage = async (e) => {
+    e.preventDefault();
 
-    //let [roomMessages, setRoomMessages]=useState([]);
-    // const[rooms, setRooms] = useState([]);
+    let name = user.displayName;
+    let date = new Date();
+    await axios.post(`/messages/${roomId}/new`, {
+      message: input,
+      name: name,
+      timestamp: date,
+      sent: true,
+      roomId: roomId,
+    });
 
-    
-    useEffect( () => {
+    setInput("");
+  };
 
-        axios.get(`/getRoomName/${roomId}`)
-        .then(response => {
-            const room = response.data;
-            console.log(room);
-            setRoomName(room.roomName);
+  useEffect(() => {
+    const pusher = new Pusher("119fa00b5b664f824337", {
+      cluster: "us3",
+    });
 
-            setMessages(room.roomMessages);
-            
-        }).catch(() => {
-            alert('error retrieving data');
-        });
-        
-    }, [roomId])
+    const channel = pusher.subscribe("rooms");
+    channel.bind("updated", (message) => {
+      // console.log(message.roomMessages[message.roomMessages.length-1]);
+      const newMessage = message.roomMessages[message.roomMessages.length - 1];
+      setMessages([...messages, newMessage]);
 
-    
-    // useEffect (() => {
-    //     axios.get(`/messages/${roomId}/sync`)
-    //     .then(response => {
-    //     console.log(response.data)
-    //     setMessages(response.data);
-    // })
-    // }, [])
+      console.log(messages);
+    });
 
-    const sendMessage = async (e) => {
-        e.preventDefault();
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
 
-        let name = user.displayName;
-        let date = new Date();
-        await axios.post(`/messages/${roomId}/new`, {
-            message: input,
-            name: name,
-            timestamp: date,
-            received: true,
-            roomId: roomId,
-        });
+  // useEffect(() => {
+  //     roomMessage= messages.map( (message) =>{
+  //         if(message.roomId === roomId) {
+  //             roomMessage+=message;
+  //             return roomMessage;
+  //         }
+  //         })
+  //         console.log(roomMessage);
 
-        setInput('');
-    }
+  // }, [])
 
-
-    useEffect( () => {
-    
-        const pusher = new Pusher('119fa00b5b664f824337', {
-        cluster: 'us3'
-        });
-
-        const channel = pusher.subscribe('rooms');
-        channel.bind('updated', (message) => {
-            // console.log(message.roomMessages[message.roomMessages.length-1]);
-            const newMessage= message.roomMessages[message.roomMessages.length-1]
-            setMessages([...messages, newMessage]);
-
-            console.log(messages);
-        });
-
-        return ()=>{
-            channel.unbind_all();
-            channel.unsubscribe();
-        }
-    
-    }, [messages])
-
-
-    // useEffect(() => {
-    //     roomMessage= messages.map( (message) =>{
-    //         if(message.roomId === roomId) {
-    //             roomMessage+=message;
-    //             return roomMessage;
-    //         }
-    //         })
-    //         console.log(roomMessage);
-      
-    // }, [])
-    
-
-    return (
-        <div className='chat'>
-            <div className='chat__header'>
-                <Avatar src={`https://avatars.dicebear.com/api/human/${Math.floor(Math.random() *5000)}.svg`}/>
-                <div className='chat__headerInfo'>
-                    <h3>{roomName}</h3>
-                    <p>Last seen at ...</p>
-                </div>
-
-                <div className='chat__headerRight'>
-                    <IconButton >
-                        <SearchOutlined />
-                    </IconButton>
-                    <IconButton>
-                        <AttachFile />
-                    </IconButton>
-                    <IconButton >
-                        <MoreVert />
-                    </IconButton>
-                </div>
-
-            </div>
-            <div className='chat__body' >               
-                {messages.map((message) => (
-                    <p className={`chat__message ${message.received && 'chat__sender'} `}>
-                        <span className='chat__name'> 
-                            {message.name}
-                        </span>
-                        {message.message}
-                        <span className='chat__timestamp'>   
-                            {message.timestamp}
-                        </span>
-                    </p>
-                ))}
-               
-            </div>
-
-            <div className='chat__footer'>
-                <InsertEmoticonIcon />
-                <form >
-                    <input 
-                        value={input}
-                        placeholder='Type a message' type='text' 
-                        onChange={e =>setInput(e.target.value)}
-                    />
-                    <button 
-                        onClick={sendMessage} 
-                        type='submit'>
-                        Send 
-                    </button>
-                </form>
-                <MicIcon />
-            </div>
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar
+          src={`https://avatars.dicebear.com/api/human/${Math.floor(
+            Math.random() * 5000
+          )}.svg`}
+        />
+        <div className="chat__headerInfo">
+          <h3>{roomName}</h3>
+          <p>Last seen at ...</p>
         </div>
-    )
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        {messages.map((message) => (
+          <p className={`chat__message ${message.sent && "chat__sender"} `}>
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">{message.timestamp}</span>
+          </p>
+        ))}
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            placeholder="Type a message"
+            type="text"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={sendMessage} type="submit">
+            Send
+          </button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
