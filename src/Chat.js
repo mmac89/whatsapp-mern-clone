7424b49f46d7899e0cc5c419b329e75b10c7bab8
Mathew <mmacelwee89@gmail.com>
2020-10-28 17:26:48 -0700

    fixed css overflow issue in chat__body

--- src/Chat.js
+++ src/Chat.js
@@ -68,11 +68,7 @@ function Chat({ user }) {
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar
-          src={`https://avatars.dicebear.com/api/human/${Math.floor(
-            Math.random() * 5000
-          )}.svg`}
-        />
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>{roomName}</h3>
           <p>Last seen at ...</p>
